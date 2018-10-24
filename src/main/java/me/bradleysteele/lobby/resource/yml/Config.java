f25ec680fbf1dcd18e620c968746b0a469eb9cb3
Bradley Steele <bradleysteele2000@gmail.com>
2018-10-24 17:15:04 +0100

    Initial commit

---
+++ src/main/java/me/bradleysteele/lobby/resource/yml/Config.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2018 Bradley Steele
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.bradleysteele.lobby.resource.yml;
+
+import com.google.common.collect.Lists;
+import me.bradleysteele.commons.resource.type.ResourceYaml;
+import me.bradleysteele.lobby.resource.ResourceType;
+import me.bradleysteele.lobby.resource.Resources;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Bradley Steele
+ */
+public enum Config {
+
+    CONFIG_VERSION("config-version", 1),
+
+    SERVER_EXCLUDED_WORLDS("server.excluded-worlds", Collections.emptyList()),
+    SERVER_DISABLE_ENTITY_SPAWN("server.disable-entity-spawn", true),
+    SERVER_DISABLE_NIGHT("server.disable-night", true),
+    SERVER_DISABLE_WEATHER("server.disable-weather", true),
+    SERVER_DISABLE_HUNGER("server.disable-hunger", true),
+    SERVER_DISABLE_DAMAGE("server.disable-damage", true),
+    SERVER_DISABLE_BLOCK_BREAK("server.disable-block-break", true),
+    SERVER_DISABLE_BLOCK_PLACE("server.disable-block-place", true),
+    SERVER_DISABLE_PICKUP("server.disable-pickup", true),
+    SERVER_DISABLE_DROP("server.disable-drop", true),
+
+    SPAWN_GAMEMODE("spawn.gamemode", "ADVENTURE"),
+    SPAWN_ON_VOID("spawn.on-void", true),
+    SPAWN_COMMAND("spawn.command", true);
+
+    public static ResourceYaml getConfig() {
+        return (ResourceYaml) Resources.get().getResource(ResourceType.CONFIG);
+    }
+
+    private final String path;
+    private final Object def;
+
+    /**
+     * @param path the path to the setting.
+     * @param def  default value of the setting.
+     */
+    Config(String path, Object def) {
+        this.path = path;
+        this.def = def;
+    }
+
+    /**
+     * @return path of the setting in config.yml.
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @return value of the path as a string, also replaces the {prefix} placeholder.
+     */
+    public String getAsString() {
+        return getConfig().getString(path, String.valueOf(def));
+    }
+
+    /**
+     * @return value of the path as a string list.
+     */
+    public List<String> getAsStringList() {
+        List<String> list = Lists.newArrayList();
+
+        if (getConfig().getConfiguration().isString(path)) {
+            list.add(getAsString());
+        } else {
+            list.addAll(getConfig().getConfiguration().getStringList(path));
+        }
+
+        return list.stream()
+                .map(s -> s.replace("{prefix}", Locale.PREFIX.getMessage().get(0)))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @return value of the path as an int.
+     */
+    public int getAsInt() {
+        return getConfig().getInt(path, Integer.parseInt(getAsString()));
+    }
+
+    /**
+     * @return default value as an integer.
+     */
+    public int getAsIntDefault() {
+        return Integer.parseInt(String.valueOf(def));
+    }
+
+    /**
+     * @return value of the path as a short.
+     */
+    public short getAsShort() {
+        return getConfig().getShort(path, Short.parseShort(getAsString()));
+    }
+
+    /**
+     * @return value of the path as a double.
+     */
+    public double getAsDouble() {
+        return getConfig().getDouble(path, Double.parseDouble(getAsString()));
+    }
+
+    /**
+     * @return value of the path as a boolean.
+     */
+    public boolean getAsBoolean() {
+        return getConfig().getBoolean(path, Boolean.parseBoolean(getAsString()));
+    }
+
+    /**
+     * @return value of the path as a configuration section.
+     */
+    public ConfigurationSection getAsConfigurationSection() {
+        return getConfig().getConfigurationSection(path);
+    }
+}
---
+++ src/main/java/me/bradleysteele/lobby/worker/WorkerServerController.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2018 Bradley Steele
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.bradleysteele.lobby.worker;
+
+import com.google.common.collect.Sets;
+import me.bradleysteele.commons.register.worker.BWorker;
+import me.bradleysteele.lobby.resource.yml.Config;
+import me.bradleysteele.lobby.util.Permissions;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Bradley Steele
+ */
+public class WorkerServerController extends BWorker {
+
+    private static final WorkerServerController instance = new WorkerServerController();
+
+    public static WorkerServerController get() {
+        return instance;
+    }
+
+    private final Set<World> excluded = Sets.newHashSet();
+
+    private WorkerServerController() {
+        this.setPeriod(6000L);
+        this.setSync(false);
+    }
+
+    @Override
+    public void run() {
+        if (Config.SERVER_DISABLE_NIGHT.getAsBoolean()) {
+            Bukkit.getWorlds().stream()
+                    .filter(this::isExcludedWorld)
+                    .forEach(world -> world.setTime(0));
+        }
+    }
+
+    @Override
+    public void onRegister() {
+        setExcludedWorlds(Config.SERVER_EXCLUDED_WORLDS.getAsStringList().stream()
+                .map(Bukkit::getWorld)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet()));
+    }
+
+    @Override
+    public void onUnregister() {
+        excluded.clear();
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_ENTITY_SPAWN.getAsBoolean()
+                && isExcludedWorld(event.getLocation().getWorld()));
+    }
+
+    @EventHandler
+    public void onWeatherChange(WeatherChangeEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_WEATHER.getAsBoolean()
+                && event.toWeatherState()
+                && isExcludedWorld(event.getWorld()));
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER
+                && Config.SERVER_DISABLE_HUNGER.getAsBoolean()
+                && isExcludedWorld(event.getEntity().getWorld())) {
+            event.setFoodLevel(20);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_DAMAGE.getAsBoolean()
+                && isExcludedWorld(event.getEntity().getWorld())
+                && event.getEntityType() == EntityType.PLAYER);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_BLOCK_BREAK.getAsBoolean()
+                && isExcludedWorld(event.getBlock().getWorld())
+                && event.getPlayer().hasPermission(Permissions.BYPASS));
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_BLOCK_PLACE.getAsBoolean()
+                && isExcludedWorld(event.getBlock().getWorld())
+                && event.getPlayer().hasPermission(Permissions.BYPASS));
+    }
+
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_PICKUP.getAsBoolean()
+                && isExcludedWorld(event.getItem().getWorld())
+                && event.getPlayer().hasPermission(Permissions.BYPASS));
+    }
+
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
+        event.setCancelled(Config.SERVER_DISABLE_DROP.getAsBoolean()
+                && isExcludedWorld(event.getPlayer().getWorld())
+                && event.getPlayer().hasPermission(Permissions.BYPASS));
+    }
+
+    /**
+     * An excluded {@link World} is a world which settings from
+     * the {@link Config} will not be applied to.
+     *
+     * @return unmodifiable set of excluded worlds.
+     */
+    public Set<World> getExcludedWorlds() {
+        return Collections.unmodifiableSet(excluded);
+    }
+
+    /**
+     * @param world the world to check.
+     * @return {@code true} if the world is excluded.
+     */
+    public boolean isExcludedWorld(World world) {
+        return excluded.contains(world);
+    }
+
+    /**
+     * @param worlds excluded worlds
+     */
+    public void setExcludedWorlds(Collection<World> worlds) {
+        excluded.clear();
+        excluded.addAll(worlds);
+    }
+
+    /**
+     * @param world the world to exclude.
+     */
+    public void addExcludedWorld(World world) {
+        excluded.add(world);
+    }
+
+    /**
+     * @param world the world to remove from exclusion.
+     */
+    public void removeExcludedWorld(World world) {
+        excluded.remove(world);
+    }
+}
