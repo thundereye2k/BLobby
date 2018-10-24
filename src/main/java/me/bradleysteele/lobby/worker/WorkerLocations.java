158c180157d1dbc5b35a8e08c6a0c1eef5ad9e06
Bradley Steele <bradleysteele2000@gmail.com>
2018-10-25 00:51:16 +0100

    Removed unneeded import

--- src/main/java/me/bradleysteele/lobby/worker/WorkerLocations.java
+++ src/main/java/me/bradleysteele/lobby/worker/WorkerLocations.java
@@ -26,7 +26,6 @@ import me.bradleysteele.lobby.util.Locations;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 
 /**
